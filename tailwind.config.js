49db8e3b705aa12d9366b8df21eb148eb6f2508f
Vikram Dhatarwal <vikk.choudhary06@gmail.com>
2026-06-29 23:08:08 +0530

    Initial React project setup with Vite and Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Space Grotesk", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
